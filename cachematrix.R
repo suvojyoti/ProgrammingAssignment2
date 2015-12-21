adb6fa13ad439b6e07c4901177ad79f9a5749459
suvojyoti <suvojyoti.chowdhury@gmail.com>
2015-12-22 03:55:20 +0530

    Fist Commit for Prog Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function returns a list of special functions where 
+## a function value is written in global environment
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## if inverse is already present, returns from cache
+  i <- x$getinverse()
+  
+  if(!is.null(i)) {
+    message("getting cached data for inverse")
+    return(i)
+  }
+  data <- x$get()
+  
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
